a288625b12524b014c5bca3abca0a4d5df210ebc
ekinertac <ekinertac@gmail.com>
2024-08-14 18:25:19 +0300

    example file content

--- htmlrepo/utils.py
+++ htmlrepo/utils.py
@@ -1,14 +1,38 @@
 import os
 
+example_file = """
+# Extensions to include
+# .py
+# .js
+# .jsx
+
+# Extensions to exclude
+!*.min.js
+!*.min.css
+
+
+# Files to ignore
+!package-lock.json
+
+# Folders to ignore
+/node_modules
+/dist
+/venv
+/.venv
+/.expo
+/migrations
+/public-api
+/htmlcov
+/docs
+/.devcontainer
+/static
+/.idea
+"""
 
 def create_default_ignore_file():
     config_path = os.path.expanduser('~/.htmlrepoignore')
 
-    # read example config file
-    with open(os.path.join(os.path.dirname(__file__), '.htmlrepoignore-example'), 'r') as file:
-        example_content = file.read()
-
     if not os.path.exists(config_path):
         with open(config_path, 'w') as file:
-            file.write(example_content)
+            file.write(example_file)
         print(f"Created default .htmlrepoignore file at {config_path}")
